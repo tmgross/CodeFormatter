2caf7829b0e7ecd3806ecf12c689125a5973dd65
tmgross <tmgross8@gmail.com>
2024-11-14 16:05:39 -0500

    added testing to compare python files before and after formatting

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,40 @@
+-- Spec.hs
+import Test.QuickCheck
+import System.IO
+import System.Directory (removeFile)
+import PythonFormatter (format)
+
+inputFile :: FilePath
+inputFile = "test/test.py"
+
+expectedFile :: FilePath
+expectedFile = "test/test_expected.py"
+
+outputFile :: FilePath
+outputFile = "test/test_formatted.py"
+
+-- | Property to test that formatting produces the expected output
+prop_formatPythonFile :: Property
+prop_formatPythonFile = ioProperty $ do
+    -- Run the formatter to produce 'formatted_test.py'
+    content <- readFile inputFile
+    formattedContent <- format content
+    writeFile outputFile formattedContent
+
+    -- Check that the output matches the expected result
+    result <- compareFiles expectedFile outputFile
+
+    -- Clean up the output file after the test
+    removeFile outputFile
+    return result
+
+-- | Helper function to compare the content of two files and return a Bool
+compareFiles :: FilePath -> FilePath -> IO Bool
+compareFiles expectedFile formattedFile = do
+    expectedContents <- readFile expectedFile
+    formattedContents <- readFile formattedFile
+    return (expectedContents == formattedContents)
+
+-- Main function to run the property test
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = quickCheck prop_formatPythonFile
--- test/test.py
+++ test/test.py
@@ -1,5 +1,16 @@
+def example_function(x,y):
+  if x>y:
+      print("x is greater than y")
+  else:
+    print("y is greater than or equal to x")
+  for i in range(0,10):
+      print(i)
 
-def func():
-    x = 10
-    y = 20
-    return x + y
+
+class ExampleClass:
+ def __init__(self, name):
+   self.name=name
+   print("ExampleClass created")
+
+    def greet(self):
+      print("Hello, " + self.name)
