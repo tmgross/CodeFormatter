ab536f3621e71053f2a8da4705b7fcbf84c97d3f
tmgross <tmgross8@gmail.com>
2024-11-14 16:27:55 -0500

    added lint data types and updated lint function to use custom lint warning types

--- src/PythonLinter.hs
+++ src/PythonLinter.hs
@@ -1,36 +1,71 @@
 module PythonLinter ( lint ) where
 
--- | Lints Python code by calling various checks
-lint :: String -> [String]
-lint code = 
-    concat [ checkUnusedImports code
-           , checkSyntaxErrors code
-           , validateNamingConventions code
-           , checkUnusedVariables code
-           , checkDeprecatedFunctions code
-           , checkMutableDefaults code
-           ]
+-- | Represents a single linting warning.
+data LintWarning = LintWarning
+  { warningType :: WarningType
+  , lineNumber  :: Int
+  , message     :: String
+  } deriving (Show, Eq)
+
+-- | Enum for different types of warnings.
+data WarningType
+  = UnusedImport
+  | SyntaxError
+  | NamingConventionViolation
+  | DeprecatedFunction
+  | MutableDefaultArgument
+  deriving (Show, Eq)
+
+-- | Represents the result of a linting process, containing all warnings and summary data.
+data LintResult = LintResult
+  { warnings     :: [LintWarning]
+  , errorCount   :: Int
+  , warningCount :: Int
+  , summary      :: String
+  } deriving (Show, Eq)
+
+-- Helper function to count errors and warnings based on WarningType
+countSeverity :: [LintWarning] -> (Int, Int)
+countSeverity = foldr (\w (eCount, wCount) ->
+            case warningType w of
+              SyntaxError -> (eCount + 1, wCount)
+              _           -> (eCount, wCount + 1)
+          ) (0, 0)
+
+-- | Lints Python code by calling various checks and returning a `LintResult`.
+lint :: String -> LintResult
+lint code =
+    let warnings = concat [ checkUnusedImports code
+                          , checkSyntaxErrors code
+                          , validateNamingConventions code
+                          , checkUnusedVariables code
+                          , checkDeprecatedFunctions code
+                          , checkMutableDefaults code
+                          ]
+        (errorCount, warningCount) = countSeverity warnings
+        summary = "Linting completed with " ++ show errorCount ++ " errors and " ++ show warningCount ++ " warnings."
+    in LintResult warnings errorCount warningCount summary
 
 -- | Checks for unused imports in Python code.
-checkUnusedImports :: String -> [String]
+checkUnusedImports :: String -> [LintWarning]
 checkUnusedImports = undefined
 
 -- | Analyzes Python code for syntax errors.
-checkSyntaxErrors :: String -> [String]
+checkSyntaxErrors :: String -> [LintWarning]
 checkSyntaxErrors = undefined
 
 -- | Validates the naming conventions in the Python code.
-validateNamingConventions :: String -> [String]
+validateNamingConventions :: String -> [LintWarning]
 validateNamingConventions = undefined
 
 -- | Checks for unused variable declarations.
-checkUnusedVariables :: String -> [String]
+checkUnusedVariables :: String -> [LintWarning]
 checkUnusedVariables = undefined
 
 -- | Checks for deprecated function usage.
-checkDeprecatedFunctions :: String -> [String]
+checkDeprecatedFunctions :: String -> [LintWarning]
 checkDeprecatedFunctions = undefined
 
 -- | Checks for mutable default arguments in function definitions.
-checkMutableDefaults :: String -> [String]
+checkMutableDefaults :: String -> [LintWarning]
 checkMutableDefaults = undefined
