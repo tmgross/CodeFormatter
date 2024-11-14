bb048878c9b013d78af9a4cc618ec943c935760d
tmgross <tmgross8@gmail.com>
2024-11-14 16:41:07 -0500

    updated formatter config options

--- src/PythonFormatter.hs
+++ src/PythonFormatter.hs
@@ -1,14 +1,18 @@
-module PythonFormatter ( format ) where
+module PythonFormatter (format, Config(..)) where
 
 import Data.Char (isSpace)
 import Data.Maybe (fromMaybe)
 import qualified Data.Map as Map
+import System.IO
 import System.Directory (doesFileExist)
 
 -- | Configuration for formatting options
-data Config = Config {
-    tabWidth :: Int
-} deriving (Show)
+data Config = Config
+  { tabWidth         :: Int
+  , maxLineLength    :: Maybe Int
+  , trimTrailingWS   :: Bool
+  , addNewlineEOF    :: Bool
+  } deriving (Show, Eq)
 
 -- | Read configuration from a file, defaulting if not found or parse error
 loadConfig :: IO Config
@@ -18,10 +22,13 @@ loadConfig = do
     then do
         content <- readFile "formatter.config"
         let settings = parseConfig content
-        return $ Config {
-            tabWidth = fromMaybe 4 (Map.lookup "tabWidth" settings >>= readMaybe)
+        return Config {
+            tabWidth = fromMaybe 4 (Map.lookup "tabWidth" settings >>= readMaybe),
+            maxLineLength = Map.lookup "maxLineLength" settings >>= readMaybe,
+            trimTrailingWS = fromMaybe True (Map.lookup "trimTrailingWS" settings >>= readBool),
+            addNewlineEOF = fromMaybe True (Map.lookup "addNewlineEOF" settings >>= readBool)
         }
-    else return $ Config 4 -- default tab width
+    else return $ Config 4 Nothing True True -- default configuration values
 
 -- | Parse configuration key-value pairs
 parseConfig :: String -> Map.Map String String
@@ -32,6 +39,12 @@ readMaybe :: Read a => String -> Maybe a
 readMaybe s = case reads s of
     [(val, "")] -> Just val
     _           -> Nothing
+
+-- | Read boolean values safely
+readBool :: String -> Maybe Bool
+readBool "true"  = Just True
+readBool "false" = Just False
+readBool _       = Nothing
 
 -- | Format Python code with loaded configuration
 format :: String -> IO String
